c3fab9bf6f0f98d8c67348d66f70c0d4f2e261b8
arunanuwantha97 <arunanuwanthablan97@gmail.com>
2021-01-27 13:38:58 +0530

    create build folder

---
+++ build/precache-manifest.3b85ea7f06e77026b43913295d997ece.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "673719670f154fe6f98027d9ea27e885",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b6bfeea6cd4741b29f0",
+    "url": "/static/css/main.c5a3ee5c.chunk.css"
+  },
+  {
+    "revision": "70564acb1319eaf60d8a",
+    "url": "/static/js/2.924bc316.chunk.js"
+  },
+  {
+    "revision": "8dbc9bd92b344a5deca62eeacc09fc2d",
+    "url": "/static/js/2.924bc316.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b6bfeea6cd4741b29f0",
+    "url": "/static/js/main.e3289101.chunk.js"
+  },
+  {
+    "revision": "deaf17d855cbdaf7344d",
+    "url": "/static/js/runtime-main.47788c5d.js"
+  },
+  {
+    "revision": "f1d71f777331fd7e3de116edf4ee3b67",
+    "url": "/static/media/avatar.f1d71f77.jpg"
+  },
+  {
+    "revision": "e52419719dd0c4c8b172136f0b625008",
+    "url": "/static/media/bg.e5241971.jpg"
+  },
+  {
+    "revision": "843781834b1db2908f08d921c6460c81",
+    "url": "/static/media/bg2.84378183.jpg"
+  },
+  {
+    "revision": "e76de24b6d8ab5e920bb4d49241ad15f",
+    "url": "/static/media/bg3.e76de24b.jpg"
+  },
+  {
+    "revision": "199e9ec14308832c6a8a49bd96c1fde6",
+    "url": "/static/media/bg4.199e9ec1.jpg"
+  },
+  {
+    "revision": "5693a33c2ef8bd6b98a73bf4a0d9c032",
+    "url": "/static/media/bg5.5693a33c.jpg"
+  },
+  {
+    "revision": "56633ed3f62f39d71f571374a6409e65",
+    "url": "/static/media/christian.56633ed3.jpg"
+  },
+  {
+    "revision": "e0508cc923eb0b5e68ca6783c53d0f1d",
+    "url": "/static/media/kendall.e0508cc9.jpg"
+  },
+  {
+    "revision": "df8fd3efcd662b64b44de07f351c838b",
+    "url": "/static/media/landing-bg.df8fd3ef.jpg"
+  },
+  {
+    "revision": "08e18cb904f0f3c6dd9e3d4ed72e0eca",
+    "url": "/static/media/landing.08e18cb9.jpg"
+  },
+  {
+    "revision": "baf6b40a654b078399e93e3d9cb6d455",
+    "url": "/static/media/profile-bg.baf6b40a.jpg"
+  },
+  {
+    "revision": "090a5aabae505f67ee0981613d02ee05",
+    "url": "/static/media/profile.090a5aab.jpg"
+  },
+  {
+    "revision": "cf8b686b294041d0925f4e745b1fabb9",
+    "url": "/static/media/sign.cf8b686b.jpg"
+  }
+]);
